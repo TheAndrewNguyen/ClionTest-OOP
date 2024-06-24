cfbef33a80ffcff6dc4c3fb01d1609463a737bec
TheAndrewNguyen <andrewbvietnguyen@gmail.com>
2024-06-24 11:18:08 -0700

    test

---
+++ cmake-build-debug/CMakeFiles/OOP_Practice.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/andre/CLionProjects/OOP_Practice/src/Book.cpp" "CMakeFiles/OOP_Practice.dir/src/Book.cpp.obj" "gcc" "CMakeFiles/OOP_Practice.dir/src/Book.cpp.obj.d"
+  "C:/Users/andre/CLionProjects/OOP_Practice/src/Libary.cpp" "CMakeFiles/OOP_Practice.dir/src/Libary.cpp.obj" "gcc" "CMakeFiles/OOP_Practice.dir/src/Libary.cpp.obj.d"
+  "C:/Users/andre/CLionProjects/OOP_Practice/src/main.cpp" "CMakeFiles/OOP_Practice.dir/src/main.cpp.obj" "gcc" "CMakeFiles/OOP_Practice.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OOP_Practice.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Practice.dir/src/Book.cpp.obj"
+  "CMakeFiles/OOP_Practice.dir/src/Book.cpp.obj.d"
+  "CMakeFiles/OOP_Practice.dir/src/Libary.cpp.obj"
+  "CMakeFiles/OOP_Practice.dir/src/Libary.cpp.obj.d"
+  "CMakeFiles/OOP_Practice.dir/src/main.cpp.obj"
+  "CMakeFiles/OOP_Practice.dir/src/main.cpp.obj.d"
+  "OOP_Practice.exe"
+  "OOP_Practice.exe.manifest"
+  "OOP_Practice.pdb"
+  "libOOP_Practice.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
